560a09f3e8c733f1d099807e58c3261786e7a582
Mjmedina <mjmedina@fullsail.edu>
2015-03-05 18:13:33 -0500

    Problem 1
    Finished first part of problem 1

--- Day2LabExpressions/js/script.js
+++ Day2LabExpressions/js/script.js
@@ -4,3 +4,15 @@ Section 00
 Day 2 Lab Expressions
  */
 
+// Calculate how much pizza everyone will eat.
+
+var slicesPizza = 8;
+var partyPeople = 8;
+var pizzaOrdered = 1;
+
+var peopleSlices = slicesPizza / partyPeople;
+var phrase = "Each person ate";
+var phrase2 = "slice at the party.";
+
+console.log(phrase, peopleSlices, phrase2);
+
