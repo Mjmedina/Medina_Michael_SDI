e83ada399a2e83c12fcdd06b8f15781173845c49
Mjmedina <mjmedina@fullsail.edu>
2015-03-05 18:36:47 -0500

    Sparky's Slices
    Worked out the second problem

--- Day2LabExpressions/js/script.js
+++ Day2LabExpressions/js/script.js
@@ -16,3 +16,15 @@ var phrase2 = "slice at the party.";
 
 console.log(phrase, peopleSlices, phrase2);
 
+
+// Calculate how much pizza Sparky will eat.
+
+var slicesPizza = 8;
+var partyPeople = 8;
+var pizzaOrdered = 1;
+
+var sparkyPizza = slicesPizza - partyPeople;
+var phrase3 = "Sparky ate";
+var phrase4 = "slices of pizza at the party";
+
+console.log(phrase3, sparkyPizza, phrase4);
