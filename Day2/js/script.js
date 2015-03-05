c78b62c880ae625a017465692d16f09eb08d261f
Mjmedina <mjmedina@fullsail.edu>
2015-03-05 14:54:01 -0500

    day2 Intro files starting
    Created html and js file for day #2

---
+++ Day2/js/script.js
@@ -0,0 +1,19 @@
+/*
+Michael Medina
+3/5/15
+Section 00
+Day 2 Intro
+ */
+
+//alert("Testing to see if connected.");
+
+/*
+Example of a multilined comment.
+Anything inside of here will not be seen by the computer.
+ */
+
+// Single line comment. ONLY this line will be ignored.
+
+//Alert box- pop up box that will alert the user with information.
+alert("Text that the user will see");
+
