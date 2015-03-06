6bcaea65480a11c521b1e0c2663cb6cb746ede07
Mjmedina <mjmedina@fullsail.edu>
2015-03-05 19:32:13 -0500

    Finished day2Lab
    Cleaned up code

--- Day2LabExpressions/js/script.js
+++ Day2LabExpressions/js/script.js
@@ -6,29 +6,19 @@ Day 2 Lab Expressions
 
 // Calculate how much pizza everyone will eat.
 
-var slicesPizza = 8;
-var partyPeople = 8;
-var pizzaOrdered = 1;
+var slicesPizza = 10;
+var partyPeople = 10;
+var pizzaOrdered = 3;
 
-var peopleSlices = slicesPizza / partyPeople;
-var phrase = "Each person ate";
-var phrase2 = "slice at the party.";
+var totalSlices = slicesPizza * pizzaOrdered;
+var slicesPerPerson = totalSlices / partyPeople;
 
-console.log(phrase, peopleSlices, phrase2);
-
+console.log("Each person ate", slicesPerPerson, "slices at the party");
 
 // Calculate how much pizza Sparky will eat.
 
-var slicesPizza = 8;
-var partyPeople = 8;
-var pizzaOrdered = 1;
-
-var sparkyPizza = slicesPizza - partyPeople;
-var phrase3 = "Sparky ate";
-var phrase4 = "slices of pizza at the party";
-
-console.log(phrase3, sparkyPizza, phrase4);
-
+var sparkyPizza = totalSlices % partyPeople;
+console.log("Sparky ate", sparkyPizza, "slices of pizza at the party");
 
 // Total and average amount of groceries over a 5 week period.
 
@@ -41,20 +31,19 @@ var week5 = 80;
 var totalGroceries = week1 + week2 + week3 + week4 + week5;
 var totalWeeks = 5;
 var average = (week1 + week2 + week3 + week4 + week5) /totalWeeks;
-var phrase5 = "You have spent a total of $";
-var phrase6= "on groceries over 5 weeks. That is an average of $";
-var phrase7= "per week.";
-console.log(phrase5, totalGroceries,phrase6, average, phrase7 );
+console.log("You have spent a total of $", totalGroceries,"on groceries over 5 weeks. That is an average of $", average,"per week");
 
 // Discounts on an item with or without tax.
 
 var originalPrice = 100;
-var discount = 20/100;
+var discount = 20;
+var appliedDiscount = discount / 100
 var item = "Ironman Helmet";
-var salesTax = 6/100;
+var salesTax = 6;
+var appliedSalesTax = salesTax /100
 
-var withTax = (originalPrice * discount) * salesTax;
-var withoutTax = originalPrice * salesTax;
-console.log("Your", item, "was originally $", originalPrice, ",but after a", discount, "% discount, " +
+var withTax = (originalPrice * appliedDiscount) * appliedSalesTax;
+var withoutTax = originalPrice * appliedSalesTax;
+console.log("Your", item, "was originally $", originalPrice, ",but after a", appliedDiscount, "% discount, " +
 "it is now $", withoutTax, "without tax, and $", withTax, "with tax.");
 
