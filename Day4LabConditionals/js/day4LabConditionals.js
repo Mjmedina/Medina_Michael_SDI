28152d7b25e6b58815c58400cde393388c40ed39
Mjmedina <mjmedina@fullsail.edu>
2015-03-11 02:00:47 -0400

    Some changes to Lab
    Lab changes

--- Day4LabConditionals/js/day4LabConditionals.js
+++ Day4LabConditionals/js/day4LabConditionals.js
@@ -10,14 +10,50 @@ Assignment
 
  //Average fuel econonomy
 
- var mpg = 30
- var gasLeft = prompt("Out of a total 100, how much gas would you estimate is left in the tank?");
- var gasMax = 100
+ var mpg = 30;
+ var gasLeft = prompt("How much gas would you estimate is left in the tank?");
+ var tankCapacity = 100;
+ var totalDistance = parseInt(mpg)*parseInt(tankCapacity);
+ var tankPercent = parseInt(totalDistance) - gasLeft;
+ var distance = totalDistance - tankPercent;
 
  // Create "if" statement for when to fill up.
 
- if(gasLeft >=35){
-     console.log("Yes, you can make it without stopping for gas!");
- } else {
-     console.log("You only have "+gasLeft+" gallons of gas left in your tank, better get gas while you can! ");
+ if(distance >=200){
+     console.log("If you have a full tank with "+mpg+"mpg and a tank capacity of"+gasLeft+"% then you do not to fill your tankd";}
+ else{
+     console.log("You need to fill your tank!");
+ }
+ //Create user login system with matching name and password.
+ var userName = prompt("Please enter your username");
+ var password = prompt("Please enter your password");
+
+ //Create "if" statement for name and password.
+ if(userName == "Tony" && password == "Skate"){
+     console.log("Access Granted");
+ }
+ else if(userName == "Tony" && password !="Skate"){
+     console.log("Access Denied because you forgot your password");
+ }else if(userName != "Tony"){
+     console.log("Access Denied because you forgot your username");
+ }
+
+
+
+ //Tire Pressure in a car
+
+ var pressureArray = [];
+
+ //Tire pressures
+
+ pressureArray[0] = prompt("How much PSI is in the front left tire?");
+ pressureArray[1] = prompt("How much PSI is in the front right tire?");
+ pressureArray[2] = prompt("How much PSI is in the back left tire?");
+ pressureArray[3] = prompt("How much PSI is in the back right tire?");
+
+
+ if((pressureArray[0] == pressureArray[1]) && (pressureArray[2] == pressureArray[3])) {
+     console.log("The tires pass spec!");
+ }else{
+     console.log("Get your tires checked!");
  }
