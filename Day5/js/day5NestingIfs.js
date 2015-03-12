bdf4e472e923903bb87379a72fc9283fc39dc68a
Mjmedina <mjmedina@fullsail.edu>
2015-03-12 15:03:10 -0400

    Learned about nesting if statements
    Nested some If’s.
    One decision affects the other

--- Day5/js/day5NestingIfs.js
+++ Day5/js/day5NestingIfs.js
@@ -8,4 +8,48 @@ Day 5 Nesting If's
 
 //alert("Test!");
 
- 
+//Some Decisions Affect Other Decisions
+
+//If it is warm then let's go to the beach, if it is not the lets go to the movies.
+
+//Get the temperature
+ var temp= 75;
+
+ //Let's set the water temperature.
+ var waterTemp = 78;
+
+ if(temp >= 80){
+     console.log("Let's go to the beach!");
+
+     if (waterTemp > 75){
+         console.log("Let's go swimming.");
+     }else {
+         console.log("Let's get a tan.");
+     }
+
+ } else{
+     console.log("Let's go to the movies!");
+
+
+
+     //Do we have any kids in the group?
+     var kids = "No";
+      //Test if we have kids.
+     if(kids ==="No"){
+         console.log("Let's see 50 Shades of Gray!");
+     } else{
+         console.log("Let's go see Spongebob!");
+     }
+
+ }
+
+/*
+// If the water temperature is above 75, let's go swimming, if not let's get a tan.
+
+
+ if (waterTemp > 75){
+     console.log("Let's go swimming.");
+ }else {
+     console.log("Let's get a tan.");
+ }
+*/
