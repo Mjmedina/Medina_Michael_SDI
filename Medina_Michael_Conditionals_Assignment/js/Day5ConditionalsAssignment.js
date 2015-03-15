3f004869abe140566bad25dbc06ffcb965c174df
Mjmedina <mjmedina@fullsail.edu>
2015-03-14 22:25:30 -0400

    Implement loops
    Looping

--- Medina_Michael_Conditionals_Assignment/js/Day5ConditionalsAssignment.js
+++ Medina_Michael_Conditionals_Assignment/js/Day5ConditionalsAssignment.js
@@ -9,7 +9,7 @@ Day 5 Conditionals Assignment
 //alert("Just testing!");
 
 
- // I may be addicted to Spelunky. We will find out. For the sake of this I will say "addiction" is more than 4 hours a day.
+ // I may be addicted to Spelunky. We will find out. For the sake of this I will say "addiction" is more than 8 hours a day.
 
  var spelunkyPlayed = prompt("We are going to figure out if you are addicted to Spelunky.\nHow many hours did you play Spelunky today?");
 
@@ -18,19 +18,40 @@ Day 5 Conditionals Assignment
 
 
 
+ if (spelunkyPlayed === "" || isNaN(spelunkyPlayed))
  //Validating that user actually typed something in the box. Because users are often dumb.
- if(spelunkyPlayed === "" || isNaN(spelunkyPlayed)){
+ while(spelunkyPlayed === "" || isNaN(spelunkyPlayed))
+ {
      spelunkyPlayed = prompt("You can't run from your problems. Answer the question.\nNow then, how many hours did you play Spelunky?");
  }
 
  //Stuff
+
+ if (spelunkyPlayed > 6){
+     console.log("You have problems. Get off the Spelunky. Go outside. Talk to people. Just get off Spelunky.");
+ } else if(spelunkyPlayed > 4){
+     console.log("You're getting close to playing too much Spelunky. But it's fine we understand, it is Spelunky after all.");
+ } else if(spelunkyPlayed >2){
+     console.log("You're fine keep playing Spelunky it's awesome!");
+ } else{
+     console.log("You should probably start playing some Spelunky before you get rusty ;)");
  }
 
- if (spelunkyPlayed >= 4){
 
 
+var questionArray =[];
 
+ //For loop to save user input in the array.
+ for(var i=1; i<6;i++)
+ {questionArray[i] = prompt("How many hours did you play on day " + i)}
 
+ //Form the array.
+ totalHours = parseInt(questionArray[1])+ parseInt(questionArray[2])+ parseInt(questionArray[3])+ parseInt(questionArray[4])+ parseInt(questionArray[5]);
 
+ //Display point totals.
+ console.log("Your total hours playing Spelunky "+totalHours);
 
- }
+ //Average
+ var averagePoints = parseFloat(totalHours)/5;
+ console.log("Your played "+averagePoints+" hours this week");
+ console.log(questionArray);
