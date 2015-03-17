7d2c883f48222660911c015278f4c660f9d8b563
Mjmedina <mjmedina@fullsail.edu>
2015-03-17 16:30:26 -0400

    finished
    sad

---
+++ Day7/js/day7Arguments.js
@@ -0,0 +1,44 @@
+
+ /*
+Michael Medina
+Date
+Section 00
+Assignment
+ */
+
+//alert("Just testing!");
+
+
+ //Arguments - Goes into the functions - part of the function call
+ //Parameters - catch the arguments and in the function definition
+
+ //Create a function that calculates teh area of a rectangle.....again
+
+ function calcArea(w,h){
+     //var width = 10;
+     //var height = 20;
+     var area=w*h;
+     console.log("The area is "+ area);
+ }
+
+ //Call our function
+ //Add arguments to the function call
+ calcArea(10,20);
+ calcArea(20,40);
+
+ //Prompt the user for width and height
+ var width = prompt("Enter a width");
+
+ //ENTER VALIDATION HERE
+var height = prompt("Enter a height");
+ calcArea(width, height);
+
+
+ //dog years
+ function dogYear(humanAge){
+     //Dog years = human years * 7
+     
+
+
+
+ }
---
+++ Day7/js/day7LoopArrays.js
@@ -0,0 +1,18 @@
+
+ /*
+Michael Medina
+Date
+Section 00
+Assignment
+ */
+
+//alert("Just testing!");
+
+// create a function that will console log out hello
+ function printHello(){
+     console.log("Hi");
+ }
+
+ //Function call the printHello function
+ // functionName();
+ printHello();
