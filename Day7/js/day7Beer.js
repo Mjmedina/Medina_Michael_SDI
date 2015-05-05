e4424daee08da2deed76eddc5acedab871dd3b9c
Mjmedina <mjmedina@fullsail.edu>
2015-05-05 09:00:03 -0400

    a
    a

--- Day7/js/day7Beer.js
+++ Day7/js/day7Beer.js
@@ -17,7 +17,7 @@ Assignment
 
  for(var i=99; i>0; i--) {
      if(i===1){
-         console.log("1 bottle of beer on the wall, 1 bottles of beer. You take one down, pass it around. No more bottles of beer on the wall.\nThe End");
+         console.log("1 bottle of beer on the wall, 1 bottles of beer. You take one down, pass it around. We're drunk and out of beer.\nThe End");
 
      }else {
          console.log(i+" bottles of beer on the wall, "+i+" bottles of beer. You take one down, pass it around. "+(i-1)+" bottles of beer on the wall");
